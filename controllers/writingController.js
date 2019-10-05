6c739848afced78e0d2b81972a77080acfa44c85
EunkyoungKim <dossiba9603@naver.com>
2019-10-05 13:51:43 +0900

    view, controller 적용

---
+++ controllers/writingController.js
@@ -0,0 +1,5 @@
+export const writing = (req, res) => res.render("writing");
+export const upload = (req, res) => res.render("upload");
+export const writingDetail = (req, res) => res.render("writingDetail");
+export const editWriting = (req, res) => res.render("editWriting");
+export const deleteWriting = (req, res) => res.render("deleteWriting");
